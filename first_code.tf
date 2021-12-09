42487c31bd869d1a413849d4ded9877e6efbdf09
zawscloud <bleahzj@gmail.com>
2021-12-08 21:38:09 -0500

    Added a simple EC2 instance to code

--- first_code.tf
+++ first_code.tf
@@ -4,10 +4,20 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-#This creates an EC2 Instance
+#This creates an S3 Bucket
 resource "aws_s3_bucket" "tf_course" {
   bucket = "tf-course-12-08-21"
   acl    = "private"
+}
+
+#This creates an EC2 Instance
+resource "aws_instance" "name" {
+  ami = "ami-002068ed284fb165b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "tf-course-webserver"
+  }
 }
 
 
